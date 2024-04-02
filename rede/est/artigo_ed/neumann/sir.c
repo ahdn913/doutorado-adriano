b93b97640dcb46ec0a157be4171387ef29aa89a2
ahdn913 <ahdn913@gmail.com>
2024-04-01 21:55:14 -0300

    updating the files

--- rede/est/artigo_ed/neumann/sir.c
+++ rede/est/artigo_ed/neumann/sir.c
@@ -1,6 +1,6 @@
-// gcc sir_vax_snap12.c -O3 -Wall -lgsl -lgslcblas -lm -o sir_vax_snap12
-// ./sir_vax_snap12
-// for I in $(seq 1 100); do echo $I; ./sir_vax_snap12 $I ;done
+// gcc sir.c -O3 -Wall -lgsl -lgslcblas -lm -o sir
+// ./sir
+// for I in $(seq 1 100); do echo $I; ./sir $I ;done
 // 1 = Suscetível, 2 = Infectado, 3 = Recuperado, 4 = Vacinado
 // argv[1] = seed
 
@@ -13,7 +13,6 @@
 #define L 200 // L = Ni = Nj
 #define Ni 200   // tamanho da rede em i
 #define Nj 200   // tamanho da rede em j
-#define repeticoes 10000
 
 int phi[L*L];
 
@@ -74,7 +73,7 @@ int count_clusters(void){
 
 int main(int argc, char **argv){
 	int i, j, x; 
-	int ativo, passivo, vizinho, pr_int, cont_inf, cont_sus, cont_s, cont_i, cont_r, cont_v;
+	int ativo, passivo, vizinho, cont_inf, cont_sus, cont_s, cont_i, cont_r, cont_v, trigger;
 	int max, t=0;
 	int a_ih, a_fh, a_iv, a_fv, key;
 	char name[100];
@@ -89,154 +88,123 @@ int main(int argc, char **argv){
 	}
 	gsl_rng *w= gsl_rng_alloc(gsl_rng_taus); 
 
-	for(epsilon= 0.0020; epsilon< 0.0021; epsilon+= 0.0002){
-		lambda= 0.85;
-		gamma= 0.15;
-		ic(phi);
-		cont_sus= 1;
-		cont_inf= 1;
-		pr_int= epsilon*10000000; //*************** diminuir 1 zero
-		op(0);
-		while(cont_sus > 0){
-			while(cont_inf > 0){
-				while(t < 500){
-					cont_s = cont_i = cont_r = cont_v = 0;
-					for(i= 0; i< L*L; i++){
-						if(phi[i] == 1){
-							cont_s++;
-						}
-						else if(phi[i] == 2){
-							cont_i++;
-						}
-						else if(phi[i] == 3){
-							cont_r++;
-						}
-						else if(phi[i] == 4){
-							cont_v++;
-						}
-					}
-					sprintf(name, "plot_curve/data-%ld.dat", gsl_rng_default_seed); // Retorna uma string de acordo com a string de formatação Format
-					arq= fopen(name, "a"); // Abre o arquivo contido no primeiro parâmetro, usando o modo dado no segundo parâmetro. com o "w", o arquivo é criado se não existir.	
-					fprintf(arq, "%d %d %d %d\n", t, cont_s, cont_i, cont_r);
-					fclose(arq);
+	sprintf(name, "curve/data.dat"); // Retorna uma string de acordo com a string de formatação Format
+	arq= fopen(name, "w"); // Abre o arquivo contido no primeiro parâmetro, usando o modo dado no segundo parâmetro. com o "w", o arquivo é criado se não existir.	
+	fclose(arq);
 
-					for(x= 0; x< Ni*Nj; x++){
-						i= gsl_rng_uniform(w)*Ni;
-						j= gsl_rng_uniform(w)*Nj;
-						ativo= i*Nj+j;
-						vizinho= gsl_rng_uniform(w)*4;
-						switch(vizinho){
-							case 0:
-								passivo= ((i+1)%Ni)*Nj+j;
-							break;
-							case 1:
-								passivo= ((i-1+Ni)%Ni)*Nj+j;
-							break;
-							case 2:
-								passivo= i*Nj+(j+1)%Nj;
-							break;
-							default:
-								passivo= i*Nj+(j-1+Nj)%Nj;
-							break;
-						}
-						acao= gsl_rng_uniform(w);
-						acao2= gsl_rng_uniform(w);
-						if(acao < lambda){
-							//infecção
-							if(phi[passivo] == 1 && phi[ativo] == 2){
-								phi[passivo]= phi[ativo];
-							}
-						}
-						if(acao2 < gamma){
-							//recuperação
-							if(phi[ativo] == 2){
-								phi[ativo]= 3;
-							}
-						}
-						/*if(phi[ativo]==1){
-							vax= gsl_rng_uniform(w);
-							if(epsilon > vax){
-								phi[ativo]=4;
-							}
-						}*/
+	trigger= 0;
+	lambda= 0.85;
+	gamma= 0.15;
+	ic(phi);
+	cont_sus= 1;
+	cont_inf= 1;
+	op(0);
+	while(cont_sus > 0){
+		while(cont_inf > 0){
+			while(t < 500){
+				cont_s = cont_i = cont_r = cont_v = 0;
+				for(i= 0; i< L*L; i++){
+					if(phi[i] == 1){
+						cont_s++;
 					}
-					cont_sus= 0;
-					for(i= 0; i < Ni; i++){
-						for(j= 0; j < Nj; j++){
-							if(phi[i*Nj+j]==1){
-								cont_sus++;
-							}
-						}
+					else if(phi[i] == 2){
+						cont_i++;
 					}
-					cont_inf= 0;
-					for(i= 0; i < Ni; i++){
-						for(j= 0; j < Nj; j++){
-							if(phi[i*Nj+j]==2){
-								cont_inf++;
-							}
-						}
+					else if(phi[i] == 3){
+						cont_r++;
 					}
-					if(cont_inf == 0){
-						if(t>30){
-							cont_sus= 0;
-							t++;
-							sprintf(name, "plot_curve/data-%ld.dat", gsl_rng_default_seed); // Retorna uma string de acordo com a string de formatação Format
-							arq= fopen(name, "a"); // Abre o arquivo contido no primeiro parâmetro, usando o modo dado no segundo parâmetro. com o "w", o arquivo é criado se não existir.	
-							fprintf(arq, "%d %d %d %d\n", t, cont_s, cont_i, cont_r);
-							fclose(arq);
-							t+=-1;
-						}
-						else{
-							ic(phi);
-							t= 0;
-							sprintf(name, "plot_curve/data-%ld.dat", gsl_rng_default_seed); // Retorna uma string de acordo com a string de formatação Format
-							arq= fopen(name, "w"); // Abre o arquivo contido no primeiro parâmetro, usando o modo dado no segundo parâmetro. com o "w", o arquivo é criado se não existir.	
-							fprintf(arq, "%d %d %d %d\n", t, cont_s, cont_i, cont_r);
-							fclose(arq);
-						}
+					else if(phi[i] == 4){
+						cont_v++;
 					}
-					t++;
-					op(t);
 				}
+				sprintf(name, "curve/data.dat"); // Retorna uma string de acordo com a string de formatação Format
+				arq= fopen(name, "a"); // Abre o arquivo contido no primeiro parâmetro, usando o modo dado no segundo parâmetro. com o "w", o arquivo é criado se não existir.	
+				fprintf(arq, "%d %d %d %d\n", t, cont_s, cont_i, cont_r);
+				fclose(arq);
+
+				for(x= 0; x< Ni*Nj; x++){
+					i= gsl_rng_uniform(w)*Ni;
+					j= gsl_rng_uniform(w)*Nj;
+					ativo= i*Nj+j;
+					vizinho= gsl_rng_uniform(w)*4;
+					switch(vizinho){
+						case 0:
+							passivo= ((i+1)%Ni)*Nj+j;
+						break;
+						case 1:
+							passivo= ((i-1+Ni)%Ni)*Nj+j;
+						break;
+						case 2:
+							passivo= i*Nj+(j+1)%Nj;
+						break;
+						default:
+							passivo= i*Nj+(j-1+Nj)%Nj;
+						break;
+					}
+					acao= gsl_rng_uniform(w);
+					acao2= gsl_rng_uniform(w);
+					if(acao < lambda){
+						//infecção
+						if(phi[passivo] == 1 && phi[ativo] == 2){
+							phi[passivo]= phi[ativo];
+						}
+					}
+					if(acao2 < gamma){
+						//recuperação
+						if(phi[ativo] == 2){
+							phi[ativo]= 3;
+						}
+					}
+					/*if(phi[ativo]==1){
+						vax= gsl_rng_uniform(w);
+						if(epsilon > vax){
+							phi[ativo]=4;
+						}
+					}*/
+				}
+				cont_sus= 0;
+				for(i= 0; i < Ni; i++){
+					for(j= 0; j < Nj; j++){
+						if(phi[i*Nj+j]==1){
+							cont_sus++;
+						}
+					}
+				}
+				cont_inf= 0;
+				for(i= 0; i < Ni; i++){
+					for(j= 0; j < Nj; j++){
+						if(phi[i*Nj+j]==2){
+							cont_inf++;
+						}
+					}
+				}
+				if(cont_inf == 0 && trigger == 0){
+					printf("O último infectado foi recuperado em t = %d. \n", t-1);
+					trigger= 1;
+				}
+				if(cont_inf == 0){
+					if(t>30){
+						cont_sus= 0;
+						t++;
+						sprintf(name, "curve/data.dat"); // Retorna uma string de acordo com a string de formatação Format
+						arq= fopen(name, "a"); // Abre o arquivo contido no primeiro parâmetro, usando o modo dado no segundo parâmetro. com o "w", o arquivo é criado se não existir.	
+						fprintf(arq, "%d %d %d %d\n", t, cont_s, cont_i, cont_r);
+						fclose(arq);
+						t+=-1;
+					}
+					else{
+						ic(phi);
+						t= 0;
+						sprintf(name, "curve/data.dat"); // Retorna uma string de acordo com a string de formatação Format
+						arq= fopen(name, "w"); // Abre o arquivo contido no primeiro parâmetro, usando o modo dado no segundo parâmetro. com o "w", o arquivo é criado se não existir.	
+						fprintf(arq, "%d %d %d %d\n", t, cont_s, cont_i, cont_r);
+						fclose(arq);
+					}
+				}
+				t++;
+				op(t);
 			}
 		}
-
-		
-	//				Percolação
-		/*
-		key= 0;
-		max= count_clusters();
-		for(i= L*L+1; i<= max; i++){ 
-			a_ih= a_fh= 0;
-			for(j= 0; j< L; j++){
-				if(phi[j] == i){ 
-					a_ih++; 
-				}
-				if(phi[(L-1)*L+j] == i){ 
-					a_fh++; 
-				}
-			}
-
-			if(a_ih > 0 && a_fh > 0){
-				key= 1;
-			}
-		}
-		
-		sprintf(name, "dat/%d_%d/percolacao_%d.dat", repeticoes, L, pr_int);
-		if(!(file= fopen(name, "a"))){
-			printf("cannot open file teste\n");
-			exit(0);
-		}
-		if(key == 1){
-			fprintf(file, "1\n");
-			printf("percolou\n");
-		}
-		else{
-			fprintf(file, "0\n");
-			printf("não percolou\n");
-		}
-		fclose(file);
-		*/
 	}
 	gsl_rng_free(w);
 	return 0;
